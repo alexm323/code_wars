dc2c8864947d4782ee6a6b18cf6c5e5307dbb472
alexm323 <alejandromartinezjr42@gmail.com>
2021-05-25 19:32:08 -0700

    harder than i thought lol

--- toSquareOrNotToSquare.js
+++ toSquareOrNotToSquare.js
@@ -1,3 +1,14 @@
 function squareOrSquareRoot(array) {
-    return array;
+    return array.map((element) => {
+        let sqrt = Math.sqrt(element)
+
+        if (sqrt % 1 === 0) {
+            return Math.sqrt(element)
+        } else {
+            return element ** 2
+        }
+
+    })
 }
+
+console.log(squareOrSquareRoot([4, 3, 9, 7, 2, 1]))
