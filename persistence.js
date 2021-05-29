99daa41346bd3251962f1346ff1c185ec3f1a505
alexm323 <alejandromartinezjr42@gmail.com>
2021-05-29 13:18:52 -0700

    code war katas 5-29-21

---
+++ persistence.js
@@ -0,0 +1,33 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example:
+
+//  persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+//                        // and 4 has only one digit
+                 
+//  persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+//                         // 1*2*6 = 12, and finally 1*2 = 2
+                  
+//  persistence(4) === 0 // because 4 is already a one-digit number
+
+function persistence(num, count = 0, persistentNum = 1){
+    if(num < 10 && count === 0) return 0; 
+    let numArr = num.toString().split('')
+    for(let num of numArr){
+        num = parseInt(num);
+        persistentNum = persistentNum*num
+    }
+    console.log(persistentNum)
+    count = count +1;
+    console.log("Count",count)
+    if(persistentNum.toString().length === 1){ 
+        return count;
+    }else{
+       return persistence(persistentNum,count)
+    }
+    
+
+    
+};
+
+persistence(39)
