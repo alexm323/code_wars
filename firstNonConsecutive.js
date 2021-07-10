95a91f30929c98574abd4129ca2d8ab5c32a51b9
alexm323 <alejandromartinezjr42@gmail.com>
2021-07-10 15:47:54 -0700

    summing arrays

--- firstNonConsecutive.js
+++ firstNonConsecutive.js
@@ -7,3 +7,15 @@
 // If the whole array is consecutive then return null2.
 
 // The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+
+function firstNonConsecutive (arr) {
+    for (let i = 0; i < arr.length -1; i++) {
+        if(arr[i] + 1 !== arr[i+1]){
+            return arr[i +1]
+            
+        }
+    }
+    return null
+  }
+
+console.log(firstNonConsecutive([1,2,3,4,6,7,8]))
