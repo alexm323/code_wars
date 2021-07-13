e55cb9a58651dc57ea1d04093e60db65962474ee
alexm323 <alejandromartinezjr42@gmail.com>
2021-07-13 08:34:28 -0700

    array in substring array

---
+++ 6kyu/arrayInArray.js
@@ -0,0 +1,15 @@
+function inArray(array1,array2){
+    let result = []
+    let og = [...array1]
+    for(let word of array2){
+      for(let w of array1){
+        if(word.includes(w)){
+          result.push(w)
+        }
+      }
+    }
+    
+    
+    
+    return og.filter(w => result.includes(w)).sort()
+  }
